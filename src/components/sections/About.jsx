b2fc25160d85ccb5852144c1f24dfbe140866081
mateusz uchyla <45610643+m-uchyla@users.noreply.github.com>
2023-04-21 11:38:24 +0200

    social media added to about section

--- src/components/sections/About.jsx
+++ src/components/sections/About.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Dubmell, Weight } from '../../assets'
+import { FaTwitterSquare, FaFacebookSquare, FaInstagramSquare  } from "react-icons/fa";
 
 const About = () => {
   return (
@@ -7,12 +8,27 @@ const About = () => {
       <div className='flex justify-between items-center'>
         <img src={Weight} className='w-[550px]'/>
         <div>
-          <h2 className='text-[48px] font-semibold text-white'>Jestem Mateold!</h2>
+          <h2 className='text-[48px] font-semibold text-secondary'>Jestem Mateold!</h2>
           <div className='bg-primary w-[500px] h-[2px]'/>
-          <p className='max-w-[585px] text-[20px] text-white mt-2'>
+          <p className='max-w-[585px] text-[20px] text-secondary mt-2'>
             Swoją przygodę ze sportami sylwetkowymi zacząłem w latach 80, co przekłada się nie tylko na wielkie bicepsy, ale też ogromne doświadczenie. Jeśli jesteś systematyczny i dążysz do wyznaczonych celów, osiągniemy je wspólnie!
           </p>
+          <div className='flex-1 flex justify-start gap-2 max-w-[585px] mt-2'>
+            <a className='cursor-pointer'>
+              <FaTwitterSquare size={50} color={"rgb(235 192 38)"}/>
+            </a>
+            <a className='cursor-pointer'>
+              <FaFacebookSquare size={50} color={"rgb(235 192 38)"} />
+            </a>
+            <a className='cursor-pointer'>
+              <FaInstagramSquare size={50} color={"rgb(235 192 38)"} />
+            </a>
+          </div>
         </div>
+      </div>
+      <div className='flex justify-between items-center'>
+
+        
       </div>
     </section>
   )
